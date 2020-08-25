5e3e0e3dfaa314916814a2c5d11c1ba5f820b3fd
Matti Paksula <matti.paksula@iki.fi>
2020-08-25 13:56:30 +0300

    index: pages

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,10 +1,31 @@
-import SuperButton from '../components/buttons/SuperButton'
+import SuperButton from '../components/buttons/SuperButton';
 import Button from '@material-ui/core/Button';
+import Link from 'next/link';
 
 export default function () {
   return (
     <main>
 
+      <h2>Pages</h2>
+      <ul>
+        <li>
+          <Link href="/another" color="secondary">
+            /another
+          </Link>
+        </li>
+        <li>
+          <Link href="/dashboard" color="secondary">
+            /dashboard
+          </Link>
+        </li>
+        <li>
+          <Link href="/healthz" color="secondary">
+            /healthz
+          </Link>
+        </li>
+      </ul>
+
+      <h2>Components</h2>
       <SuperButton style="okay">HELLO</SuperButton>
       <SuperButton style="error">WORLD</SuperButton>
 
