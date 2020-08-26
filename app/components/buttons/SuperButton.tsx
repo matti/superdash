cd88f47528f88a8b634dc3b39be7b78d2b55a414
Matti Paksula <matti.paksula@iki.fi>
2020-08-26 12:17:40 +0300

    superbutton as ts

--- app/components/buttons/SuperButton.tsx
+++ app/components/buttons/SuperButton.tsx
@@ -1,11 +1,21 @@
+import React from 'react'
 import styles from './SuperButton.module.css'
-export default function SuperButton(props) {
-  return (
-    <button
-      type="button"
-      className={styles.container + " " + styles[props.style]}
-    >
-      {props.children}
-    </button>
-  )
+interface Props {
+  style: string;
 }
+
+class SuperButton extends React.Component<Props> {
+  render() {
+    return (
+      <button
+        type="button"
+        className={styles.container + " " + styles[this.props.style]}
+      >
+        {this.props.children}
+      </button>
+    )
+  }
+
+}
+
+export default SuperButton
